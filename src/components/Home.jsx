cc0e1c0b25eb37872e03bcde8e379ac0e1251b99
whatkimkong <whatkimkong@gmail.com>
2023-02-08 21:53:18 +0000

    update: cleanup landing layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,16 +11,10 @@ export class Home extends Component {
     return (
       <div>
         <img src={title} alt="title" className="root-title" />
-
         <div className="root-text">
-          <h3>DIY straight from your Pantry</h3>
-          <h5>
-            Sharing our recipes from far and wide in the six categories of{" "}
-          </h5>
-          <br />
-          <h2>CLEANING • PLAY • FOOD • DRINK • BODYCARE • FACECARE</h2>
-          <br />
-          <h5>to meet our needs in fresh new ways.</h5>
+          <h6 className="home-txt">DIY with everyday ingredients and share your recipes for: </h6>
+          <h2 className="home-txt">CLEANING • PLAY • FOOD • DRINK • BODYCARE • FACECARE</h2>
+          <h6 className="home-bottom">Go on, bet Grandma has some wise wisdoms we can rescue....</h6>
           { !user && (
             <div className="root-button-container">
             <NavLink className="root-submit" to="/signup">
@@ -33,7 +27,6 @@ export class Home extends Component {
               find out more...
             </NavLink>
           </div>
-          <h5 className="page-bottom">Go on, bet Grandma has some wise wisdoms we can rescue....</h5>
         </div>
       </div>
     );
